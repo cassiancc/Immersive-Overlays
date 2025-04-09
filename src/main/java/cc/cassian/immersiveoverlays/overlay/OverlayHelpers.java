9c307b2ec49f70ce1330e38a570805fecb20bf35
Cassian Godsted <cassian@godsted.com>
2025-04-09 18:07:28 -0400

    Refix detection code

--- src/main/java/cc/cassian/immersiveoverlays/overlay/OverlayHelpers.java
+++ src/main/java/cc/cassian/immersiveoverlays/overlay/OverlayHelpers.java
@@ -12,7 +12,6 @@ import net.minecraft.client.gui.GuiGraphics;
 /*import net.minecraft.client.gui.GuiComponent;
 import com.mojang.blaze3d.vertex.PoseStack;
  *///?}
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceLocation;
@@ -92,51 +91,43 @@ public class OverlayHelpers {
         return debug && !mc.options.reducedDebugInfo().get();
     }
 
+    private static void getHandContainerContents(ItemStack handItem) {
+        List<ItemStack> list = getContents(handItem).toList();
+        for (ItemStack itemStack : list) {
+            if (itemStack.is(ModTags.SHOWS_XZ))
+                CompassOverlay.showXZ = true;
+            else if (itemStack.is(ModTags.SHOWS_Y))
+                CompassOverlay.showY = true;
+            else if (itemStack.is(ModTags.SHOWS_TIME))
+                ClockOverlay.showTime = true;
+            else if (itemStack.is(ModTags.SHOWS_WEATHER))
+                ClockOverlay.showWeather = true;
+        }
+    }
+
     public static void checkInventoryForItems(Player player) {
         if (player == null)
             return;
         if (ModConfig.get().compass_enable || ModConfig.get().clock_enable) {
-            var main = player.getMainHandItem();
-            var offhand = player.getOffhandItem();
             var inventory = player.getInventory();
-            boolean inHand = ModConfig.get().require_item_in_hand;
-            // Compass (X/Z)
-            if (main.is(ModTags.SHOWS_XZ) || offhand.is(ModTags.SHOWS_XZ)) {
-                CompassOverlay.showXZ = true;
-            } else if (inHand) {
-                CompassOverlay.showXZ = checkInventoryForItem(inventory, ModTags.SHOWS_XZ);
-            }
-            // Depth Gauge (Y)
-            if (main.is(ModTags.SHOWS_Y) || offhand.is(ModTags.SHOWS_Y)) {
-                CompassOverlay.showY = true;
-            } else if (inHand) {
-                CompassOverlay.showY = checkInventoryForItem(inventory, ModTags.SHOWS_Y);
-            }
-            // Clock (Time)
-            if (main.is(ModTags.SHOWS_TIME) || offhand.is(ModTags.SHOWS_TIME)) {
-                ClockOverlay.showTime = true;
-            } else if (inHand) {
-                ClockOverlay.showTime = checkInventoryForItem(inventory, ModTags.SHOWS_TIME);
-            }
-            // Barometer
-            if (main.is(ModTags.SHOWS_WEATHER) || offhand.is(ModTags.SHOWS_WEATHER)) {
-                ClockOverlay.showWeather = true;
-            } else if (inHand) {
-                ClockOverlay.showWeather = checkInventoryForItem(inventory, ModTags.SHOWS_WEATHER);
+            var mainhand = player.getMainHandItem();
+            var offhand = player.getOffhandItem();
+            if (ModConfig.get().require_item_in_hand) {
+                CompassOverlay.showXZ = mainhand.is(ModTags.SHOWS_XZ) || offhand.is(ModTags.SHOWS_XZ);
+                CompassOverlay.showY = mainhand.is(ModTags.SHOWS_Y) || offhand.is(ModTags.SHOWS_Y);
+                ClockOverlay.showTime = mainhand.is(ModTags.SHOWS_TIME) || offhand.is(ModTags.SHOWS_TIME);
+                ClockOverlay.showWeather = mainhand.is(ModTags.SHOWS_WEATHER) || offhand.is(ModTags.SHOWS_WEATHER);
+                if (mainhand.is(ModTags.CONTAINERS)) {
+                    getHandContainerContents(mainhand);
+                }
+            } else {
+                CompassOverlay.showXZ = checkInventoryForItem(inventory, ModTags.SHOWS_XZ, offhand.is(ModTags.SHOWS_XZ));
+                CompassOverlay.showY = checkInventoryForItem(inventory, ModTags.SHOWS_Y, offhand.is(ModTags.SHOWS_Y));
+                ClockOverlay.showTime = checkInventoryForItem(inventory, ModTags.SHOWS_TIME, offhand.is(ModTags.SHOWS_TIME));
+                ClockOverlay.showWeather = checkInventoryForItem(inventory, ModTags.SHOWS_WEATHER, offhand.is(ModTags.SHOWS_WEATHER));
             }
             if (offhand.is(ModTags.CONTAINERS)) {
-                List<ItemStack> list = getContents(offhand).toList();
-                for (ItemStack itemStack : list) {
-                    if (itemStack.is(ModTags.SHOWS_XZ))
-                        CompassOverlay.showXZ = true;
-                    else if (itemStack.is(ModTags.SHOWS_Y))
-                        CompassOverlay.showY = true;
-                    else if (itemStack.is(ModTags.SHOWS_TIME))
-                        ClockOverlay.showTime = true;
-                    else if (itemStack.is(ModTags.SHOWS_WEATHER))
-                        ClockOverlay.showWeather = true;
-                }
-
+                getHandContainerContents(offhand);
             }
         } else {
             CompassOverlay.showXZ = false;
@@ -177,10 +168,9 @@ public class OverlayHelpers {
     }
 
 
-    public static boolean checkInventoryForItem(Inventory inventory, TagKey<Item> item) {
-        if (inventory.contains(item)) {
-            return true;
-        }
+    public static boolean checkInventoryForItem(Inventory inventory, TagKey<Item> item, boolean value) {
+        if (value) return true;
+        if (inventory.contains(item)) return true;
         else return checkInventoryForStack(inventory, item, null) != ItemStack.EMPTY;
     }
 
