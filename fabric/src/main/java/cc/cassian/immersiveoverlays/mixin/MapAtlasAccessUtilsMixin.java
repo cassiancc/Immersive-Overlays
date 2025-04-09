fe49309e1f7f5e1eb37ab13bbc936130c34b3115
Cassian Godsted <cassian@godsted.com>
2025-04-08 22:53:26 -0400

    Revert "Further offhand fixes"
    This reverts commit 200421179ef0217b518258648bd0c79b3f3c20aa.

--- fabric/src/main/java/cc/cassian/immersiveoverlays/mixin/MapAtlasAccessUtilsMixin.java
+++ fabric/src/main/java/cc/cassian/immersiveoverlays/mixin/MapAtlasAccessUtilsMixin.java
@@ -1,10 +1,8 @@
 package cc.cassian.immersiveoverlays.mixin;
 
 
-import cc.cassian.immersiveoverlays.ModTags;
 import cc.cassian.immersiveoverlays.overlay.OverlayHelpers;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -19,28 +17,16 @@ import pepjebs.mapatlases.utils.MapAtlasesAccessUtils;
 public class MapAtlasAccessUtilsMixin {
     //? if >1.20 {
     @Inject(method = "getAtlasFromInventory", at = @At(value = "RETURN"), remap = false, cancellable = true)
-    private static void getAtlasFromBundle(Inventory inventory, boolean onlyHotbar, CallbackInfoReturnable<ItemStack> cir) {
+    private static void mixin(Inventory inventory, boolean onlyHotbar, CallbackInfoReturnable<ItemStack> cir) {
         if (cir.getReturnValue().equals(ItemStack.EMPTY)) {
             cir.setReturnValue(OverlayHelpers.checkInventoryForStack(inventory, null, MapAtlasesMod.MAP_ATLAS.get()));
         }
     }
-    @Inject(method = "getAtlasFromPlayerByConfig", at = @At(value = "HEAD"), remap = false, cancellable = true)
-    private static void getAtlasFromBundle(Player player, CallbackInfoReturnable<ItemStack> cir) {
-        if (player.getOffhandItem().is(ModTags.CONTAINERS)) {
-            cir.setReturnValue(OverlayHelpers.checkInventoryForStack(player.getOffhandItem(), null, MapAtlasesMod.MAP_ATLAS.get()));
-        }
-    }
     //?} else {
     /*@Inject(method = "getAtlasFromInventory", at = @At(value = "RETURN"), remap = false, cancellable = true)
-    private static void getAtlasFromBundle(Inventory inventory, CallbackInfoReturnable<ItemStack> cir) {
+    private static void mixin(Inventory inventory, CallbackInfoReturnable<ItemStack> cir) {
         if (cir.getReturnValue() == null) {
             cir.setReturnValue(OverlayHelpers.checkInventoryForStack(inventory, null, MapAtlasesMod.MAP_ATLAS));
-        }
-    }
-    @Inject(method = "getAtlasFromPlayerByConfig", at = @At(value = "HEAD"), remap = false, cancellable = true)
-    private static void getAtlasFromBundle(Player player, CallbackInfoReturnable<ItemStack> cir) {
-        if (player.getOffhandItem().is(ModTags.CONTAINERS)) {
-            cir.setReturnValue(OverlayHelpers.checkInventoryForStack(player.getOffhandItem(), null, MapAtlasesMod.MAP_ATLAS));
         }
     }
     *///?}
