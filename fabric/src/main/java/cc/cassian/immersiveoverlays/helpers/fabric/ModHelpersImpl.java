f3a5d24843bdb0d7b574e5d0153293275bcbb91c
Cassian Godsted <cassian@godsted.com>
2025-09-26 14:23:56 -0400

    release 1.3.1

--- fabric/src/main/java/cc/cassian/immersiveoverlays/helpers/fabric/ModHelpersImpl.java
+++ fabric/src/main/java/cc/cassian/immersiveoverlays/helpers/fabric/ModHelpersImpl.java
@@ -3,8 +3,11 @@ package cc.cassian.immersiveoverlays.helpers.fabric;
 import cc.cassian.immersiveoverlays.config.ModConfig;
 import cc.cassian.immersiveoverlays.overlay.TemperatureOverlay;
 import net.fabricmc.fabric.api.tag.client.v1.ClientTags;
+//? if >1.21 {
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBiomeTags;
-import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
+//?} else {
+/*import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
+*///?}
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
@@ -20,9 +23,19 @@ public class ModHelpersImpl {
     }
 
     public static TemperatureOverlay.TemperaturePair getBiomeTemperatureFromTag(Holder<Biome> biomeHolder) {
-        if (ClientTags.isInWithLocalFallback(ConventionalBiomeTags.IS_HOT, biomeHolder)) {
+        if (ClientTags.isInWithLocalFallback(
+                //? if >1.21
+                ConventionalBiomeTags.IS_HOT
+                //? if <1.21
+                /*ConventionalBiomeTags.CLIMATE_HOT*/
+                ,biomeHolder)) {
             return new TemperatureOverlay.TemperaturePair(Component.translatable("gui.c.temperature.hot"), ModConfig.get().temperature_hot_colour, "heat_4");
-        } else if (ClientTags.isInWithLocalFallback(ConventionalBiomeTags.IS_COLD, biomeHolder)) {
+        } else if (ClientTags.isInWithLocalFallback(
+                //? if >1.21
+                ConventionalBiomeTags.IS_COLD
+                //? if <1.21
+                /*ConventionalBiomeTags.CLIMATE_COLD*/
+                , biomeHolder)) {
             return new TemperatureOverlay.TemperaturePair(Component.translatable("gui.c.temperature.cold"), ModConfig.get().temperature_cold_colour, "heat_0");
         } else return new TemperatureOverlay.TemperaturePair(Component.translatable("gui.c.temperature.temperate"), ModConfig.get().temperature_temperate_colour, "heat_1");
     }
